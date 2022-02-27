7f481967f9b2a34ccdfeee56c1f3d47fd032623c
riunosk <riunosk@outlook.com>
2022-02-27 16:17:27 +0800

    scene change for main menu + instruction button (kyle)
    scene script change (kyle)

---
+++ osuHOWProject/Assets/Scripts/MainMenuInstructionButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuInstructionButton : MonoBehaviour
+{
+	public void InstructionScene()
+	{
+		SceneManager.LoadScene("InstructionScene");
+	}
+}
